af23dccf75fecf2082d4960ee6ce9648a048a46e
Nhung Nguyen <nhungnguyentester@gmail.com>
2023-09-29 23:06:59 +0700

    update lab-3

--- Lab/Lab3_03.java
+++ Lab/Lab3_03.java
@@ -2,10 +2,19 @@ import java.util.Arrays;
 
 public class Lab3_03 {
     public static void main(String[] args) {
-        int[] intArrSort = new int[] {12, 34, 1, 16, 28};
-        Arrays.sort(intArrSort);
-        for ( int a : intArrSort){
-            System.out.println(a);
+        int[] intArrSort = new int[]{12, 34, 1, 16, 28};
+        for (int i = 0; i < intArrSort.length; i++) {
+            for (int j = 0; j < intArrSort.length - 1; j++) {
+                int temp = intArrSort[j];
+                if (intArrSort[j] < intArrSort[j + 1]) {
+                    intArrSort[j] = intArrSort[j + 1];
+                    intArrSort[j + 1] = temp;
+                }
+            }
         }
-    }}
+        for (int i : intArrSort) {
+            System.out.println(i);
+        }
+    }
+}
 
