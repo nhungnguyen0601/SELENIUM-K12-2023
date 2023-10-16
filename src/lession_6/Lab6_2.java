56612bc87d2925bad9568c87421d957f42c0bbc1
Nhung Nguyen <nhungnguyentester@gmail.com>
2023-10-16 22:14:06 +0700

    lab 6.1 and 6.2

---
+++ src/lession_6/Lab6_2.java
@@ -0,0 +1,32 @@
+package lession_6;
+
+import java.util.Scanner;
+
+public class Lab6_2 {
+    public static void main(String[] args) {
+        int maxInput = 3;
+        int trial = 0;
+        String correctPassword = "password123";
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (trial < maxInput) {
+            System.out.print("Enter your password: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals(correctPassword)) {
+                System.out.println("Login successful!");
+                break;
+            } else {
+                trial++;
+                System.out.println("Incorrect password.");
+            }
+        }
+
+        if (trial == maxInput) {
+            System.out.println("Maximum attempts reached. Access denied.");
+
+        }
+
+    }
+}
